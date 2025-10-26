68654ed45185e0e92eb1bb88be6b170c1b95c060
LMC\aurelien.boisselet <aurelien.boisselet@lmcfrance.com>
2025-10-27 00:10:40 +0100

    base de donnees

--- wp-content/plugins/lmc-multistep-form/src/actions/step_7.php
+++ wp-content/plugins/lmc-multistep-form/src/actions/step_7.php
@@ -1,38 +1,6 @@
 <?php
 
-/*
- * Token CSRF
- */
-if (!isset($_POST['step6_csrf_token']) || $_POST['step6_csrf_token'] !== $_SESSION['lmc_data']['csrf_token']) {
-    $_SESSION['lmc_data']['error_step'] = 1;
-    $_SESSION['lmc_data']['$error_message'] = "Requête invalide.";
-    lmc_multistep_form__logLmc("step7 Token CSRF invalide");
-    die();
-}
-
-/*
- * Honey Pot pour piéger les robots
- */
-if (!empty($_POST['step6_honeypot'])) {
-    $_SESSION['lmc_data']['error_step'] = 1;
-    $_SESSION['lmc_data']['$error_message'] = "Robot détecté..";
-    lmc_multistep_form__logLmc("step7 Honey Pot rempli (robot détecté)");
-    die();
-}
-
-
-/*
- * Vérification du mail
- */
-if( !isset($_SESSION['lmc_data']['step3_2fa']) || $_SESSION['lmc_data']['step3_2fa'] != 1){
-    $_SESSION['lmc_data']['error_step'] = 1;
-    $_SESSION['lmc_data']['$error_message'] = "Votre Email n'est pas vérifié.";
-    lmc_multistep_form__logLmc("step7 Votre Email n'est pas vérifié.)");
-    die();
-}
-
-$messageFin = "Votre formulaire a déjà été envoyé.";
-$_SESSION['lmc_data']['reload'] = null;
+$messageFin = "Votre demande d'adhésion a été prise en compte.";
 $data = $_SESSION['lmc_data'];
 $data['confirmation'] = 'OK';
 
@@ -53,9 +21,6 @@ if (count($step1_results) === 1) {
     lmc_multistep_form__logLmc("step4 Votre formulaire n\'a pas été envoyé.)");
     die();
 }
-
-
-
 
 
 ?>
